5587646801ea2c6c271ad0cea379a4cbd567c55d
aqu1no1 <aquinomauricio101@gmail.com>
2026-01-14 21:15:29 -0300

    beecrowd | 1041 Coordenadas de um Ponto

---
+++ b41.js
@@ -0,0 +1,24 @@
+var input = require("fs").readFileSync("entrada.txt", "utf8");
+//var input = require("fs").readFileSync("/dev/stdin", "utf8");
+var lines = input.split("\n");
+
+let [x, y] = lines[0].trim().split(" ");
+
+x = parseFloat(x);
+y = parseFloat(y);
+
+if (x === 0 && y === 0) {
+  console.log(`Origem`);
+} else if (x === 0) {
+  console.log(`Eixo Y`);
+} else if (y === 0) {
+  console.log(`Eixo X`);
+} else if (x > 0 && y > 0) {
+  console.log(`Q1`);
+} else if (x < 0 && y > 0) {
+  console.log(`Q2`);
+} else if (x < 0 && y < 0) {
+  console.log(`Q3`);
+} else if (x > 0 && y < 0) {
+  console.log(`Q4`);
+}
