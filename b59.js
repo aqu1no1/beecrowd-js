5b37bac1e2d01bba1ca502ad958f9a801101b93c
aqu1no1 <aquinomauricio101@gmail.com>
2026-01-28 21:19:32 -0300

    beecrowd | 1059 Números Pares

---
+++ b59.js
@@ -0,0 +1,9 @@
+var input = require("fs").readFileSync("entrada.txt", "utf8");
+//var input = require("fs").readFileSync("/dev/stdin", "utf8");
+var lines = input.split("\n");
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
