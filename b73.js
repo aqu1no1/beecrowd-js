26d1e271d572592e0b17f96a7b83c047e8688b44
Mauricio Aquino <aquinomauricio101@gmail.com>
2026-02-18 22:56:09 -0300

    beecrowd | 1073 Quadrado de Pares

---
+++ b73.js
@@ -0,0 +1,13 @@
+var input = require("fs").readFileSync("entrada.txt", "utf8");
+//var input = require("fs").readFileSync("/dev/stdin", "utf8");
+var lines = input.split("\n");
+
+let num1 = parseInt(lines[0]);
+let calc = 0;
+
+for (let i = 1; i <= num1; i++) {
+  if (i % 2 === 0) {
+    calc = i * i;
+    console.log(`${i}^2 = ${calc}`);
+  }
+}
